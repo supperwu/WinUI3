5926d902d461336253af9e0313735b75a04a19d4
Jerry <supperwu4781@hotmail.com>
2024-04-07 10:26:50 +0800

    Init photo organized project

---
+++ PhotoOrganized/PhotoOrganized/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+
+
+namespace PhotoOrganized;
+
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class App : Application
+{
+    /// <summary>
+    /// Initializes the singleton application object.  This is the first line of authored code
+    /// executed, and as such is the logical equivalent of main() or WinMain().
+    /// </summary>
+    public App()
+    {
+        this.InitializeComponent();
+        var serviceProvider = new ServiceCollection()
+            .BuildServiceProvider();
+        Ioc.Default.ConfigureServices(serviceProvider);
+    }
+
+    /// <summary>
+    /// Invoked when the application is launched.
+    /// </summary>
+    /// <param name="args">Details about the launch request and process.</param>
+    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+    {
+        _window = new MainWindow();
+        _window.Activate();
+    }
+
+    private Window _window;
+}
---
+++ PhotoOrganized/PhotoOrganized/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+using Microsoft.UI.Xaml;
+
+
+namespace PhotoOrganized;
+
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+
+}
